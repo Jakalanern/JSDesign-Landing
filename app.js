52414c9e93c25acfa8aebd5c291e5bf962baeaa3
Jack Stevens <77250250+Jakalanern@users.noreply.github.com>
2021-10-31 22:55:59 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-let hi = document.querySelector(".hi");
-let jack = document.querySelector(".name");
-let bio = document.querySelector(".bio");
 let arrow = document.querySelector("#arrow1");
 let arrow2 = document.querySelector("#arrow2");
-let nav = document.querySelector("nav");
-let welcome = document.querySelector(".welcome");
 let burger = document.querySelector(".burger");
 let dropdown = document.querySelector(".dropdown");
 const tl = gsap.timeline({ default: { ease: "power1.out" } });
 
 // EVENTS
 arrow.addEventListener("click", function () {
-  tl.to("#fullscreen1", { duration: 1, y: "-100%" });
+  tl.to(".main-box", { duration: 1, y: "-100%" });
   tl.to("#fullscreen2", { duration: 1, y: "0%" }, "-=1");
 });
 
@@ -28,7 +23,7 @@ burger.addEventListener("click", function () {
 
 // // ANIMATIONS
 //BEFORES
-gsap.to(nav, { duration: 0, opacity: 1 });
+gsap.to("nav", { duration: 0, opacity: 0 });
 gsap.to("#arrow1", { duration: 0, opacity: 0 });
 gsap.to(".name", { duration: 0, x: "100vw" });
 gsap.to(".portrait", { duration: 0, x: "100vw" });
@@ -43,6 +38,6 @@ tl.to(".hide", { duration: 1, y: "0%" }, "-=1");
 tl.to(".portrait", { duration: 1, x: "0%" }, "-=1.5");
 tl.to(".name", { duration: 1, x: "0%" }, "-=1");
 tl.to("p", { duration: 1, x: "0%" });
-tl.to(nav, { duration: 1, opacity: 1 });
+tl.to("nav", { duration: 1, opacity: 1 });
 tl.to("#arrow1", { duration: 1, opacity: 1 }, "-=1");
 gsap.to(".portrait", { duration: 2, boxShadow: "0px 0px 30px #8cc8ff" }, "-=2");
